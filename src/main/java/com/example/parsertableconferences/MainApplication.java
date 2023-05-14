a20af199d60fccba5c7e221abdb13d81280f31a3
arina742 <arinazav02@gmail.com>
2023-05-14 15:09:48 +0300

    1. Изменен интерфейс 2. Заменена ссылка на сайт городзовёт

--- src/main/java/com/example/parsertableconferences/MainApplication.java
+++ src/main/java/com/example/parsertableconferences/MainApplication.java
@@ -1,11 +1,8 @@
 package com.example.parsertableconferences;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +11,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("form.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 600);
 
         stage.setTitle("Winston Churchill");
         stage.setScene(scene);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,8 @@ module com.example.ParserTableConferences {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
+//    requires com.dlsc.formsfx;
+//    requires org.kordamp.bootstrapfx.core;
     requires org.jsoup;
 
     opens com.example.parsertableconferences to javafx.fxml;
