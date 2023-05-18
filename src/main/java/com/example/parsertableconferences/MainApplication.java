71e3ef4fd71cca91ca8a13e17e0b6cb554129317
AmusedCreator <v.ilya512@gmail.com>
2023-05-18 18:05:29 +0300

    1. Перевод интерфейса

--- src/main/java/com/example/parsertableconferences/MainApplication.java
+++ src/main/java/com/example/parsertableconferences/MainApplication.java
@@ -11,7 +11,12 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("form.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+
+        stage.setMinHeight(600);
+        stage.setMinWidth(900);
+        stage.setMaxHeight(601);
+        stage.setMaxWidth(901);
 
         stage.setTitle("Winston Churchill");
         stage.setScene(scene);
